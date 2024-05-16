112ec5a8a0d87edfd322c177445590a666e60a5d
devfastated <businessmailromer@gmail.com>
2024-05-17 01:40:50 +0800

    learned migrate:fresh

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
-@include ('header')
-
-<h1>This is the about page</h1>
-<a href="/homepage">Home</a>
+<x-layout>
+    <h1>This is the about page</h1>
+    <a href="/homepage">Home</a>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ExampleController::class,'index']);
 
-Route::get('/homepage', [ExampleController::class,'newRoute']);
+Route::get('/', [ExampleController::class,'newRoute']);
 Route::get('/about', [ExampleController::class,'aboutPage']);
 Route::get('/single-post', [ExampleController::class,'singlePostPage']);
 
